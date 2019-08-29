ddd82d6f1edbf1e163819ed3f09ceebcc438420a
Mark Jarvis <mtgjarvis@gmail.com>
2019-08-29 14:13:20 -0400

    removed odd numbers from array

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function removeOddSum() {
+function removeOddSum(numList) {
+    numList.forEach((element, index) => {
+        if ((element.a + element.b) % 2 !== 0) {
+            numList.splice(index, 1);
+        }
+    })
+    return numList;
 }
 
 module.exports = removeOddSum;
